7196eae2bc27ae11baf5a5749ce822c438a8f101
Casey <c.m.s.lee0207@gmail.com>
2023-02-23 15:55:25 -0500

    day three complete up to part 6

--- script.js
+++ script.js
@@ -15,27 +15,52 @@ document.addEventListener("DOMContentLoaded", function(event) {
   randomQuote();
   
   // Do all of your work inside the document.addEventListener  
-
+  
   // Part 1
-
-
+  const mainTitle = document.getElementById('main-title')
+  mainTitle.textContent = "DOM Toretto"
+  
   // Part 2
-
-
+  const body = document.querySelector('body')
+  body.style.backgroundColor = '#B8860B'
+  
   // Part 3
-
-
+  const donsFaveThings = document.getElementById('favorite-things');
+  donsFaveThings.lastElementChild.remove()
+  
   // Part 4
-
-
+  const specialTitle = document.querySelectorAll('.special-title');
+  specialTitle.forEach((element) => {
+    element.style.fontSize = '2rem';
+  })
+    
   // Part 5
-
-
+  const pastRaces = document.getElementById('past-races');
+  if(pastRaces.hasChildNodes()){
+    pastRaces.removeChild(pastRaces.children[3]);
+  }
+  
   // Part 6
-
+  const newCityNode = document.createElement("li");
+  newCityNode.innerHTML = "Macau";
+  pastRaces.appendChild(newCityNode);
 
   // Part 7
 
+  const newDivBlogPost = document.createElement('div');
+  newDivBlogPost.classList.add('blog-post', 'purple')
+  const newH2 = document.createElement('h2');
+  newH2.innerHTML = 'Macau'
+  const newPara = document.createElement('p');
+  newPara.innerHTML = 'I almost skidded into a casino!';
+
+  newDivBlogPost.append(newH2, newPara)
+  document.body.appendChild(newDivBlogPost)
+  console.log(newDivBlogPost)
+
+
+  // append Div to Body
+  // add ul and li as well ?
 
   // Part 8
 
